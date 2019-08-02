1528f0b9827159e299c54520bb04f1983d95b309
Daniil <daniil-rostov@list.ru>
2019-08-02 13:51:05 +0300

    last

--- static/themes/js/scripts.js
+++ static/themes/js/scripts.js
@@ -1,19 +1,38 @@
 $(document).ready(function(){
-
-//	$().addcart(product);
-	
+	var form = $('#form_buying_product');
+	form.on('submit', function(e){
+		e.preventDefault();
+		
+		var nmb = $('#number').val();
+		
+		var submit_btn = $('#submit_btn');
+		var product = submit_btn.data("product_id");
+		if (nmb == 0) {
+			alert("Укажите количество");
+		} else {console.log(product+' '+nmb+' штуки.');}
+		
+	})
 });
-var i = 1;
-var prod = new Map();
 
+
+
+
+var i = 1;
+var sum = 0
+var prod = new Map();
 function addcart(product) {
 	
 	if( prod.has(product) ) {
-		n = prod.get(product);
+		var n = prod.get(product);
+		
 		prod.set(product, n+1);
 	} else 
 		{prod.set(product, i);}
 		   
-document.getElementById(count).value = 9;
-}
+prod.forEach(function(value,key) {
+	console.log('Количество = ' + key +' ' +value+ ' штук');   
+	//sum += +value;
+	//console.log(value);
+});
+};
 
